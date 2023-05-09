e821c6afdd5614745bbbf35ed0911be9a2499b33
Rcontre360 <rcontreraspimentel@gmail.com>
2023-05-09 12:34:56 -0400

    feat: added take off image

--- shared/components/common/Navbar/index.tsx
+++ shared/components/common/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import {useRouter} from "next/router";
+import Image from "next/image"; // import Image component
 
-import Image from "next/image";
 import EthAccount from "@shared/components/common/EthAccount";
 
 const Navbar = () => {
@@ -9,7 +9,12 @@ const Navbar = () => {
   return (
     <nav className="flex justify-end items-center p-1 bg-neutral-50 absolute w-full py-6 text-black">
       <div className="absolute left-16 top-1 flex items-center gap-2 h-full">
-        <span className="text-xl font-semibold">TALENT PROTOCOL</span>
+        <Image
+          src="/multimedia/assets/TAKEOFF_LOGO_purple.png" // replace this with your image path
+          alt="Talent Protocol"
+          width={120} // replace with your image's width
+          height={60} // replace with your image's height
+        />
       </div>
       <div className="w-full flex justify-end items-center w-auto">
         {router.route !== "/" && <EthAccount className="mr-10" />}
